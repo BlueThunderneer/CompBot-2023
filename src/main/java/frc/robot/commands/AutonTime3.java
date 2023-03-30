766976b3e66c1b72346202468abd86d40bf8670f
BlueThunderneer <81537440+BlueThunderneer@users.noreply.github.com>
2023-03-30 12:33:08 -0400

    Changed auton stuff

--- src/main/java/frc/robot/commands/AutonTime3.java
+++ src/main/java/frc/robot/commands/AutonTime3.java
@@ -12,18 +12,18 @@ public class AutonTime3 extends SequentialCommandGroup {
    */
   public AutonTime3(ClawSS m_ClawSS, DriveSubsystem m_drivetrain, ArmSS m_armss) {
        addCommands(
-        new armdownTime(0.7, m_armss), // Lowers arm because yes
+        new armupTime(0.5, m_armss), // Lowers arm because yes
         //new armupTime(3.0, m_armss), //Raise the Cargo arm to shoot need to add this subsystem to the contstructor)
         new extendTime(1.7, m_armss),
         new ItsClawinTime(1.0, m_ClawSS), // Not a Morbius reference (Opens le claw)
         new retractTime(1.3, m_armss),
         //new Time(1),     //One Second Delay
         //new ArmdownTime(1.0, m_ArmSS),
-        new armupTime(0.8, m_armss), //Raise the Cargo arm to shoot need to add this subsystem to the contstructor)
+        new armdownTime(0.8, m_armss), //Raise the Cargo arm to shoot need to add this subsystem to the contstructor)
         //new armdownTime(0.5, m_armss),
         //new DriveDistance( 0.75, 0, 216, m_drivetrain )
-        new DriveTime( -0.6, 0.0, 2.0, m_drivetrain) // go backwards for X seconds
-        // Caffiene addiction lvl 100
+        new DriveTime( 0.6, 0.0, 3.0, m_drivetrain) // go backwards for X seconds
+        // Caffiene addiction lvl 1000
         ); //end of addCommands
      
   }    
